74ebf6cbddf96743a111fe05af4567da34228480
GilTorch <infotorch2014@gmail.com>
2023-07-29 20:23:05 -0400

    Add legends

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ const {
     geoPath,
     geoGraticule,
     active,
-    scaleTreshold,
+    scaleLinear,
+    scaleThreshold,
     interpolateYlGn,
-    scaleSequential
+    scaleSequential,
+    format, 
+    axisBottom,
 } = d3
 
+console.log('Hello World')
 
-const width = 900;
-const height = 600;
+const width = window.innerWidth*0.6;
+const height = window.innerHeight*0.6;
 
 
-const USEducationData = 
+const margin = {
+    top: 50, 
+    left: 50, 
+    right: 50, 
+    bottom: 200
+}
+
+
+const usEducationData = 
 [
 "https://cdn.freecodecamp.org/",
 "testable-projects-fcc/",
@@ -25,7 +37,7 @@ const USEducationData =
 "for_user_education.json"
 ].join('')
 
-const USCountyData = [
+const usCountyData = [
     "https://cdn.freecodecamp.org/",
     "testable-projects-fcc/",
     "data/",
@@ -36,16 +48,15 @@ const USCountyData = [
 const svg = select('body')
     .append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', height + 200)
 
     var projection = geoAlbersUsa()
-    // .center([2, 47])                // GPS of location to zoom on
-    // .scale(980)                       // This is like the zoom
+
     .translate([ width/2, height/2 ])
 
     const path = geoPath()
     // Load external data and boot
-Promise.all([json(USCountyData),json(USEducationData)])
+Promise.all([json(usCountyData),json(usEducationData)])
   .then(([us, usEducation]) => {
     
     console.table(us)
@@ -53,39 +64,87 @@ Promise.all([json(USCountyData),json(USEducationData)])
 
     const g = svg.append('g')
 
-        // Draw the map
-        const nation = topojson.feature(us,us.objects.nation).features;
-        const states = topojson.feature(us,us.objects.states).features;
-        const counties = topojson.feature(us,us.objects.counties).features;
-
-        console.log('counties', counties)
-        const fetchCountyBachelorInfo = (id) => {
-            const county = usEducation.find(el => el.fips === id);
-            console.log('County', county)
-            if(county){
-                return county.bachelorsOrHigher;
-            }
-            return null;
+    // Draw the map
+    const nation = topojson.feature(us,us.objects.nation).features;
+    const states = topojson.feature(us,us.objects.states).features;
+    const counties = topojson.feature(us,us.objects.counties).features;
+    
+    const fetchCountyBachelorInfo = (id) => {
+        const county = usEducation.find(el => el.fips === id);
+        if(county){
+            return county.bachelorsOrHigher;
         }
+        return null;
+    }
 
-        const colorScale = scaleSequential(interpolateYlGn)
-                                    .domain([0, 100]);
+    const colorScale = scaleSequential()
+                        .interpolator(d => {
+                            return interpolateYlGn(d)
+                        })
+                        .domain([0, 100]);
 
-        const paths = g.selectAll('path');
-    
-        paths
-         .data(counties)
-         .join('path')
-         .attr('d', path)
-         .attr('fill',d => colorScale(fetchCountyBachelorInfo(d.id)))
+    const paths = g.selectAll('path');
 
-        paths
-         .data(states)
-         .join('path')
-         .attr('d', path)
-         .attr('stroke','white')
-         .attr('stroke-width',1.5)
-         .attr('fill', 'transparent')
-    
-    })
+    paths
+        .data(nation)
+        .join('path')
+        .attr('d', path)
+        .attr('stroke','black')
+        .attr('stroke-width', 5)
+
+    paths
+        .data(counties)
+        .join('path')
+        .attr('d', path)
+        .attr('fill',d => colorScale(fetchCountyBachelorInfo(d.id)))
+
+    paths
+        .data(states)
+        .join('path')
+        .attr('d', path)
+        .attr('stroke','#fff')
+        .attr('stroke-width',1.5)
+        .attr('fill', 'transparent')
+
+
+    // Legend section
+
+    const tresholdValues = [[3,12],[12,21],[21,30],[30,39],[39,48],[48,57],[57,66]];
+    const uniqueTresholdValues = new Set(tresholdValues.flat());
+    const tickValues = Array.from(uniqueTresholdValues);
+
+    const domain = [
+        Math.min(...tickValues),
+        Math.max(...tickValues)
+    ]
+    console.log(`Domain`, domain)
+
+    const legendScale = scaleLinear()
+                            .domain(domain)
+                            .range([0, 500])
+
+    const legendAxis = axisBottom(legendScale)
+
+
+   const legend = svg
+     .append('g')
+     .attr('transform', `translate(${margin.left},${height+ 100})`)
+     .call(legendAxis
+            .tickValues(tickValues)
+            .tickSizeOuter(0)
+        )
+
+    const legendHeight = 10;
+
+    legend 
+        .selectAll('.legends')
+        .data(tresholdValues)
+        .join('rect')
+        .attr('x', (d, i) => legendScale(tickValues[i]))
+        .attr('y',-legendHeight)
+        .attr('height', legendHeight)
+        .attr('width', (d,i) => legendScale(d[1]) - legendScale(d[0]))
+        .attr('fill', d => colorScale(d[0]))
+})
+
 
